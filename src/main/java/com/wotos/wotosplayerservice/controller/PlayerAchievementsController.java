4e9ac05f20a93ef338247c9c33cc0b93a869cc04
Kevin Lago <kevinthelago@gmail.com>
2022-09-15 22:23:03 -0400

    Moved Achievements To Separate Files

---
+++ src/main/java/com/wotos/wotosplayerservice/controller/PlayerAchievementsController.java
@@ -0,0 +1,33 @@
+package com.wotos.wotosplayerservice.controller;
+
+import com.wotos.wotosplayerservice.dao.PlayerAchievementsSnapshot;
+import com.wotos.wotosplayerservice.service.PlayerAchievementsService;
+import com.wotos.wotosplayerservice.service.PlayerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/player")
+public class PlayerAchievementsController {
+
+    @Autowired
+    PlayerAchievementsService playerAchievementsService;
+
+    @GetMapping("/achievements")
+    public Map<Integer, List<PlayerAchievementsSnapshot>> getPlayerAchievementsByAccountIds(
+            @RequestParam("accountIds") Integer[] accountIds
+    ) {
+        return playerAchievementsService.getPlayerAchievementsSnapshotsByAccountIds(accountIds);
+    }
+
+    @PostMapping("/achievements")
+    public Map<Integer, PlayerAchievementsSnapshot> createPlayerAchievementsByAccountIds(
+            @RequestParam("accountIds") Integer[] accountIds
+    ) {
+        return playerAchievementsService.createPlayerAchievementsSnapshotsByAccountIds(accountIds);
+    }
+
+}
